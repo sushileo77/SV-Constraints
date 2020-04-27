9aad84e473b17a56db35b29f5506ce51177df6a5
Sushant Sakhalkar <sushant.sakhalkar@gmail.com>
2020-04-26 17:44:04 -0700

    Random Number Gen Constraint Consecutive/ Non-Con
    - Generate a Random Number where just 4 consecutive numbers are random
    - However, those random numbers in the list of 4 can be repeated in further randomization calls 
    
    E.g.
        [1,4,5,8] -> These 4 are random numbers
    
        1,4,58,9,12,4 -> However the number 4 which was present earlier in the list before can repeat and that repeat should be non-consecutive

---
+++ Random_Numbers_with_4_consecutive_unique.sv
@@ -0,0 +1,35 @@
+// FB question
+// Only 4 elements need to be unique, there after they can repeat
+
+
+class ABC;
+  rand bit[4:0] item;
+  bit[4:0] item_q[$];
+  int count=0;
+  
+  constraint item_c {
+    !(item inside {item_q}); 
+  }
+  
+  function void post_randomize();
+    item_q.push_back(item);
+	if(item_q.size()==4) item_q.delete();
+    
+    $display("Item=%0d",item);
+  endfunction
+endclass : ABC
+
+module test;
+  initial begin
+    ABC abc;
+    abc=new();
+    
+    repeat(20)begin
+      abc.randomize();
+    end  
+    
+  end
+  
+endmodule
+
+
